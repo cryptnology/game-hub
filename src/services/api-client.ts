521d48475eb074f6ed09caefd734bdc0ed2a65a2
cryptnology <jamie@cryptnology.dev>
2023-03-24 12:44:12 +0000

    Create and use .env file for API

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '5fd5d93d4116444ca58ca490d3a730b3',
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
 
