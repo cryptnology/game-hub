5a6d1475cdf1cb093937a4156c022e051a7687f1
cryptnology <jamie@cryptnology.dev>
2023-03-24 10:27:43 +0000

    Create a custom hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '5fd5d93d4116444ca58ca490d3a730b3',
   },
 });
+
+export default apiClient;
