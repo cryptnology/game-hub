89a5adfa0f02ad2cb32cbf0fe4d9d222e888d3dd
cryptnology <jamie@cryptnology.dev>
2023-04-26 09:10:40 +0100

    Refactor move entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,12 +3,7 @@ import ms from 'ms';
 
 import APIClient from '../services/api-client';
 import genres from '../data/genres';
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
+import { Genre } from '../entities/Genre';
 
 const apiClient = new APIClient<Genre>('/genres');
 
