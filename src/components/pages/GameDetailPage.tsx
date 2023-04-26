67a745894c9431a5e8b4a1a60d21b49edfd5bd8a
cryptnology <jamie@cryptnology.dev>
2023-04-26 09:25:28 +0100

    Build Expandable text

--- src/components/pages/GameDetailPage.tsx
+++ src/components/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router-dom';
-import { Heading, Spinner, Text } from '@chakra-ui/react';
+import { Heading, Spinner } from '@chakra-ui/react';
 
 import useGame from '../../hooks/useGame';
+import ExpandableText from '../ExpandableText';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,7 +15,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
