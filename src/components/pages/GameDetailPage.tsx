edaf7e2edbc483b4c86bf7793240774f63cfff23
cryptnology <jamie@cryptnology.dev>
2023-04-26 09:54:08 +0100

    Build game attributes

--- src/components/pages/GameDetailPage.tsx
+++ src/components/pages/GameDetailPage.tsx
@@ -1,8 +1,9 @@
-import { useParams } from 'react-router-dom';
 import { Heading, Spinner } from '@chakra-ui/react';
+import { useParams } from 'react-router-dom';
 
 import useGame from '../../hooks/useGame';
 import ExpandableText from '../ExpandableText';
+import GameAttributes from '../GameAttributes';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -16,6 +17,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
