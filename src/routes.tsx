126ad8d5af94a2bce1df2c10f8fcc4d868aaf3cb
cryptnology <jamie@cryptnology.dev>
2023-04-26 08:13:29 +0100

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,11 +3,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import Layout from './components/pages/Layout';
 import HomePage from './components/pages/HomePage';
 import GameDetailPage from './components/pages/GameDetailPage';
+import ErrorPage from './components/pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: 'games/:id', element: <GameDetailPage /> },
